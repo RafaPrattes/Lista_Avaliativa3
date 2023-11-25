86d9cb264fc6e22f785cd0f266f1243c07b841b5
Rafaela <142021465+RafaPrattes@users.noreply.github.com>
2023-11-25 19:25:23 -0300

    Questao3.c

--- questao3.c
+++ questao3.c
@@ -1,81 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-int placaValida(char placa[]) {
-    if (strlen(placa) != 7 && strlen(placa) != 8) {
-        return 0;
-    }
-
-    for (int i = 0; i < 3; i++) {
-        if (!isalpha(placa[i])) {
-            return 0;
-        }
-    }
-
-    if (placa[3] != '-' || !isdigit(placa[4]) || !isdigit(placa[5]) || !isdigit(placa[6])) {
-        return 0;
-    }
-
-    if (strlen(placa) == 8 && !isdigit(placa[7])) {
-        return 0;
-    }
-
-    return 1;
+int formatoPlacaValido(char *placa) {
+    return (strlen(placa) == 8 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
+            isdigit(placa[4]) && isdigit(placa[5]) && isdigit(placa[6]) && isdigit(placa[7]) &&
+            placa[3] == '-') ||
+           (strlen(placa) == 7 && isalpha(placa[0]) && isalpha(placa[1]) && isalpha(placa[2]) &&
+            isdigit(placa[3]) && isalpha(placa[4]) && isdigit(placa[5]) && isdigit(placa[6]));
 }
 
-int diaSemanaValido(char diaSemana[]) {
-    char diasValidos[7][15] = {"segunda-feira", "terca-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sabado", "domingo"};
+int verificaRestricaoCirculacao(char *placa, char *diaSemana) {
+    int ultimoDigito = placa[strlen(placa) - 1] - '0';
 
-    for (int i = 0; i < 7; i++) {
-        if (strcasecmp(diaSemana, diasValidos[i]) == 0) {
-            return 1;
-        }
+    if(strcmp(diaSemana, "SEGUNDA-FEIRA") == 0) {
+        return ultimoDigito != 0 && ultimoDigito != 1;
+    } else if(strcmp(diaSemana, "TERCA-FEIRA") == 0) {
+        return ultimoDigito != 2 && ultimoDigito != 3;
+    } else if(strcmp(diaSemana, "QUARTA-FEIRA") == 0) {
+        return ultimoDigito != 4 && ultimoDigito != 5;
+    } else if(strcmp(diaSemana, "QUINTA-FEIRA") == 0) {
+        return ultimoDigito != 6 && ultimoDigito != 7;
+    } else if(strcmp(diaSemana, "SEXTA-FEIRA") == 0) {
+        return ultimoDigito != 8 && ultimoDigito != 9;
+    } else if(strcmp(diaSemana, "SABADO") == 0 || strcmp(diaSemana, "DOMINGO") == 0) {
+        return -1;
+    } else {
+        return -2;
     }
-
-    return 0;
 }
 
-int podeCircular(char letraDia, int ultimoDigito) {
-    switch (letraDia) {
-        case 's':
-            return (ultimoDigito != 0 && ultimoDigito != 1);
-        case 't':
-            return (ultimoDigito != 2 && ultimoDigito != 3);
-        case 'q':
-            return (ultimoDigito != 4 && ultimoDigito != 5);
-        default:
-            return 1;
+void converterMinusculo(char *diaSemana) {
+    for (int i = 0; i < strlen(diaSemana); i++) {
+        diaSemana[i] = tolower(diaSemana[i]);
     }
 }
 
 int main() {
     char placa[9];
-    char diaSemana[15];
+    char diaSemana[30];
 
-    scanf("%s %s", placa, diaSemana);
+    scanf("%s", placa);
+    scanf(" %s", diaSemana);
+    if (formatoPlacaValido(placa)) {
+        int resultadoRestricao = verificaRestricaoCirculacao(placa, diaSemana);
 
-    for (int i = 0; diaSemana[i]; i++) {
-        diaSemana[i] = tolower(diaSemana[i]);
-    }
-
-    if (!placaValida(placa)) {
-        printf("Placa invalida\n");
-    } else if (!diaSemanaValido(diaSemana)) {
-        printf("Dia da semana invalido\n");
-    } else {
-        int ultimoDigito = placa[strlen(placa) - 1] - '0';
-
-        if (strcasecmp(diaSemana, "sabado") == 0 || strcasecmp(diaSemana, "domingo") == 0) {
-            printf("Nao ha proibicao no fim de semana\n");
-        } else {
-            char letraDia = tolower(diaSemana[0]);
-
-            if (podeCircular(letraDia, ultimoDigito)) {
+        switch (resultadoRestricao) {
+            case 1:
+                converterMinusculo(diaSemana);
                 printf("%s pode circular %s\n", placa, diaSemana);
-            } else {
+                break;
+            case 0:
+                converterMinusculo(diaSemana);
                 printf("%s nao pode circular %s\n", placa, diaSemana);
-            }
+                break;
+            case -1:
+                printf("Nao ha proibicao no fim de semana\n");
+                break;
+            case -2:
+                printf("Dia da semana invalido\n");
+                break;
+        }
+    } else {
+        printf("Placa invalida\n");
+        if(verificaRestricaoCirculacao(placa, diaSemana) == -2) {
+            printf("Dia da semana invalido\n");
         }
     }
 
